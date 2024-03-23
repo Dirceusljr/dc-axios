3612f007333b2103a2f4cd8dd5934d50fbed3b58
dirceusljr <dirceusljr@gmail.com>
2024-03-22 21:33:26 -0300

    projeto-final

--- src/pages/Conteudo/index.jsx
+++ src/pages/Conteudo/index.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { ThemeContext } from '../../contexts/ThemeContext'
+import axios from 'axios';
 
 const Conteudo = () => {
+  const {theme, toggleTheme} = useContext(ThemeContext);
+  const [digimons, setDigimons] = useState([]);
+
+  useEffect(() => {
+    const fetchDigimons = async () => {
+      const response = await axios.get('https://digimon-api.vercel.app/api/digimon');
+      setDigimons(response.data)
+    }
+
+    fetchDigimons();
+  },[])
+
   return (
-    <div>Conteudo</div>
+    <div style={{backgroundColor: theme.background, color: theme.text}}>
+      <h2>Listagem de Digimons</h2>
+      <input type="checkbox" onChange={toggleTheme} />
+
+      <div style={{display: 'flex', flexWrap: 'wrap'}}>
+        {digimons.map((item) => (
+          <div key={item.name}style={{width: '200px', margin: '10px'}}>
+            <img src={item.img} alt={item.name} style={{width: '100%'}}/>
+            <p>{item.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,6 @@ const Home = () => {
   const {theme, toggleTheme} = useContext(ThemeContext);
   return (
     <div style={{backgroundColor: theme.background, color: theme.text}}>
-      <button onClick={toggleTheme}>Dark Mode</button>
       <h1>Bem-vindo à API do Digimon</h1>
 
       <label>
